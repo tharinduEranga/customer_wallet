2bf4ed5090c3c59d37664af75aa4139e578536ad
Tharindu Eranga <tharindu.e@bsfdv.com>
2024-01-02 20:03:45 +0400

    add project structure

---
+++ src/main/java/com/example/hubpay/wallet/CustomerWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.hubpay.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hubpay/wallet/CustomerWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hubpay.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
